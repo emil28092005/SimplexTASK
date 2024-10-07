c7bc14208163651cd65e39e00b37ae68b5909369
emil <emil28092005@gmail.com>
2024-10-07 13:24:54 +0300

    some error checks made by Vladimir

--- main.cpp
+++ main.cpp
@@ -34,21 +34,35 @@ int main() {
 
     Vector b = {24, 6, 1, 2};
 
+    for (int i = 0; i < b.size(); i++) {
+        if (b[i] < 0) {
+            std::cout << "Error: method is not applicable" << std::endl;
+            return 1;
+        }
+    }
+
     //Matrix test = {{1, -1, -2}, {1, 1, -2}, {1, -1, 2}};
 
     //showMatrix(test);
     
 
     Result result = Simplex(C, A, b, 0.1, true);
-    if(result.state == bounded) {
 
+
+
+    if(result.state == bounded) {
         if(result.solution == nullptr) {
-            // TODO: error
+            std::cout << "Error: no solution value is returned" << std::endl;
+            return 1;
         }
         std::cout << *result.solution << std::endl;
         std::cout << result.objective_fucntion_value << std::endl;
 
         delete result.solution;
+    }
+    else {
+        std::cout << "Error: unbounded" << std::endl;
+        return 1;
     }
     //std::cout << "asdasd" << std::endl;
 
